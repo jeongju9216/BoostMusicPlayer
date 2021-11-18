f9202205a06bc63b7c97e0ed282c254a50156592
jeong9216 <dbwjdwn9216@gmail.com>
2021-11-18 20:13:18 +0900

    화면 구성

--- BoostMusicPlayer/ViewController.swift
+++ BoostMusicPlayer/ViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: - IBOutlet
+    @IBOutlet var playPuaseButton: UIButton! //재생,일시정지 버튼
+    @IBOutlet var timeLabel: UILabel! //시간초 레이블
+    @IBOutlet var progressSlider: UISlider! //진행 슬라이더
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
